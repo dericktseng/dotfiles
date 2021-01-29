acee1d9cd22518dc3cb140efe29b522e421f3e2f
Derick Tseng <derickboss1@gmail.com>
2021-01-28 23:07:19 -0800

    fix temperature reporting

--- .config/polybar/thermalcpu.sh
+++ .config/polybar/thermalcpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-info=$(sensors | grep CPUTIN | tail -n 1 | cut -d '+' -f 2 | cut -d ' ' -f 1)
+info=$(sensors | grep 'k10temp-pci-00c3' -A2 | tail -n 1 | cut -d '+' -f 2 | cut -d ' ' -f 1)
 
 if [[ -z $info ]]; then
     info="$(sensors | grep pch_cannonlake-virtual-0 -A2 | tail -n 1 | cut -d '+' -f 2 | cut -d ' ' -f 1) C"
