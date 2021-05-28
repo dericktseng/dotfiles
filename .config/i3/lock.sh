5aeb29f6cc65b2b2eb36bbba251771e6570c2c52
Derick Tseng <derickboss1@gmail.com>
2021-05-28 00:08:18 -0700

    update i3lock

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -3,21 +3,21 @@
 # locks the current session with i3-lock with the given settings
 lock() {
     i3lock -nek -c "#e3e3e1ff" \
-        --insidevercolor=fdfdfddd \
-        --insidewrongcolor=fdfdfddd \
-        --insidecolor=fdfdfddd \
-        --ringvercolor=bedcd9dd \
-        --ringwrongcolor=f3a111dd \
-        --ringcolor=e2e2e2dd \
-        --keyhlcolor=083bb9dd \
-        --bshlcolor=AAAAAAdd \
-        --separatorcolor=00000000 \
-        --linecolor=00000000 \
-        --timestr="%I:%M %p" \
+        --insidever-color=fdfdfddd \
+        --insidewrong-color=fdfdfddd \
+        --inside-color=fdfdfddd \
+        --ringver-color=bedcd9dd \
+        --ringwrong-color=f3a111dd \
+        --ring-color=e2e2e2dd \
+        --keyhl-color=083bb9dd \
+        --bshl-color=AAAAAAdd \
+        --separator-color=00000000 \
+        --line-color=00000000 \
+        --time-str="%I:%M %p" \
         --indicator \
         --line-uses-inside \
-        --datestr="%A, %b %d" \
-        --wrongtext="incorrect" \
+        --date-str="%A, %b %d" \
+        --wrong-text="incorrect" \
         --radius=120 \
         --nofork
 }
