17a99fe300e6a66a2c482788d1896ac50e30d11a
Derick Tseng <derickboss1@gmail.com>
2021-06-01 23:50:04 -0700

    update imageview to support gifs

--- .config/ranger/imageview.sh
+++ .config/ranger/imageview.sh
@@ -2,6 +2,7 @@
 if [ -f "$@" ]; then
     parentdir="$(dirname "$@")"
     case "$@" in
+        *.gif)  mpv --loop "$@";;
         *.svg)  feh -F --conversion-timeout 1 -S filename . --start-at "$@";;
             *)  feh -FZ --scale-down -Sfilename "$parentdir" --start-at "$@";;
     esac
