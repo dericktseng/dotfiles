e4e4d0664215f040449601679acc4b1cd3300d70
Derick Tseng <derickboss1@gmail.com>
2021-06-06 01:00:19 -0700

    polybar

--- .config/polybar/thermalcpu.sh
+++ .config/polybar/thermalcpu.sh
@@ -3,7 +3,7 @@
 info=$(sensors | grep 'k10temp-pci-00c3' -A2 | tail -n 1 | cut -d '+' -f 2 | cut -d ' ' -f 1)
 
 if [[ -z $info ]]; then
-    info="$(sensors | grep pch_cannonlake-virtual-0 -A2 | tail -n 1 | cut -d '+' -f 2 | cut -d ' ' -f 1) C"
+    info="$(sensors | grep pch_cannonlake-virtual-0 -A2 | tail -n 1 | cut -d '+' -f 2 | cut -d ' ' -f 1)"
 fi
 
 echo " $info "
