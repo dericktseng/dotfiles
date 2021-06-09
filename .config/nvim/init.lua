55eeb98afea811d53136c78b17e0f90c964f14e0
Derick Tseng <derickboss1@gmail.com>
2021-06-08 19:44:06 -0700

    switch to lua config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,82 @@
+-- preload calls
+vim.g.loaded_python_provider = 0
+vim.g.loaded_ruby_provider   = 0
+vim.g.loaded_node_provider   = 0
+vim.g.loaded_perl_provider   = 0
+vim.g.python3_host_prog      = '/usr/bin/python3'
+
+---- colorscheme and true color settings
+vim.api.nvim_command('filetype plugin indent on')
+vim.api.nvim_command('syntax enable')
+if (vim.fn.has("termguicolors") and vim.env.TERM ~= "linux") then
+    vim.o.cursorline = true
+    vim.o.termguicolors = true
+    vim.o.background = 'light'
+    require('customlight')
+else
+    vim.opt.background = 'dark'
+end
+
+-- load plugins before anything else
+require('plugins')
+
+-- Plugin local configs
+require('compeconfig')
+require('functions')
+
+-- LSP servers
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.clangd.setup{}
+require'lspconfig'.vimls.setup{}
+
+-- Plugins Settings
+vim.api.nvim_command('source ~/.config/nvim/markdown-preview.vim')
+vim.api.nvim_command('source ~/.config/nvim/vimtex.vim')
+vim.api.nvim_command('source ~/.config/nvim/lightline.vim')
+
+-- Personal shortcuts
+vim.api.nvim_command('source ~/.config/nvim/shortcuts.vim')
+
+
+-- per filetype settings
+vim.api.nvim_command('autocmd FileType help wincmd L')
+vim.api.nvim_command('autocmd FileType tex setlocal spell spelllang=en_us')
+
+-- REST OF VIM SETTINGS
+---- tab settings
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+
+---- set line numbers
+vim.o.number = true
+vim.o.relativenumber = true
+
+---- allow mouse input and clipboard
+vim.o.mouse = 'a'
+vim.o.clipboard = 'unnamedplus'
+
+---- hides the bottom line since it is covered by lightline
+vim.o.showmode = false
+
+---- sets the cursor to have a buffer. High number to always be centered.
+vim.o.so = 10
+
+---- only show statusline when multiple windows open
+vim.o.laststatus = 1
+
+---- proper linebreaks
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.showbreak = "|"
+
+---- confirmation when actions fail
+vim.o.confirm = true
+
+---- shortmessage (set shortmess += c)
+vim.opt.shortmess:append('c')
+
+---- completeopt
+vim.opt.completeopt = {'menuone', 'noselect'}
---
+++ .config/nvim/lua/customlight.lua
@@ -0,0 +1,210 @@
+-- Remove all existing highlighting and set the defaults.
+vim.api.nvim_command('hi clear')
+
+-- set colorscheme's name
+vim.g.colors_name = "customlight"
+
+-- terminal colors
+vim.g.terminal_color_0  = '#dff4ff'
+vim.g.terminal_color_1  = '#ff8671'
+vim.g.terminal_color_2  = '#14a57c'
+vim.g.terminal_color_3  = '#499ec8'
+vim.g.terminal_color_4  = '#59b6ea'
+vim.g.terminal_color_5  = '#ee22bc'
+vim.g.terminal_color_6  = '#8f5eba'
+vim.g.terminal_color_7  = '#4a545b'
+vim.g.terminal_color_8  = '#97a5ab'
+vim.g.terminal_color_9  = '#ff8671'
+vim.g.terminal_color_10 = '#14a57c'
+vim.g.terminal_color_11 = '#499ec8'
+vim.g.terminal_color_12 = '#59b6ea'
+vim.g.terminal_color_13 = '#ee22bc'
+vim.g.terminal_color_14 = '#8f5eba'
+vim.g.terminal_color_15 = '#4a545b'
+
+-- local variables colors to insert into the highlighting function
+local white = '#eeeeee'
+local offwhite = '#dedede'
+local darkred = '#af0000'
+local turquoise = '#00a979'
+local green = '#008700'
+local lightgreen = '#46d211'
+local darkgreen = '#006200'
+local teal = '#4badb9'
+local darkgrey = '#878787'
+local lightblue = '#03b9d5'
+local black = '#222222'
+local grey = '#bcbcbc'
+local lightgrey = '#d3d3d3'
+local red = '#ff0000'
+local purple = '#a300f4'
+local darkblue = '#005faf'
+local orange = '#ea8a00'
+local yelloworange = '#daa500'
+local yellow = '#ffc900'
+local highlightyellow='#fff641'
+local blue = '#0787a9'
+local purpleblue = '#4078f2'
+local indigo = '#002ba9'
+local pink = '#f400f4'
+local skyblue = '#a0dee8'
+local brown = '#883f16'
+
+-- formatting styles
+local bold='bold'
+local italic='italic'
+local underline='underline'
+local undercurl='undercurl'
+local none='NONE'
+
+-- highlighting function. AT LEAST ONE OF fg, bg, attr, attrsp MUST NOT BE NIL
+local highlightfunc = function(group, fg, bg, attr, attrsp)
+    local foreground = fg and 'guifg=' .. fg or ''
+    local background = bg and 'guibg=' .. bg or ''
+    local style = attr and 'gui=' .. attr or ''
+    local stylesp = attrsp and 'guisp=' .. attrsp or ''
+    vim.api.nvim_command('highlight ' .. group .. ' ' .. foreground .. ' ' .. background .. ' ' .. style .. ' ' .. stylesp)
+end
+
+local linkfunc = function(group1, group2)
+    vim.api.nvim_command('highlight link ' .. group1 .. ' ' .. group2)
+end
+
+-- builtin highlight groups according to help: highlight-groups
+highlightfunc('ColorColumn', nil, darkgrey, nil, nil)
+highlightfunc('Conceal', purpleblue, white, nil, nil)
+highlightfunc('Cursor', white, black, nil, nil)
+highlightfunc('CursorColumn', nil, darkgrey, nil, nil)
+highlightfunc('CursorLine', nil, white, none, nil)
+highlightfunc('Directory', darkblue, nil, nil, nil)
+
+-- Diff
+highlightfunc('DiffAdded', green, nil, bold, nil)
+highlightfunc('DiffRemoved', darkred, nil, bold, nil)
+highlightfunc('DiffAdd', green, nil, nil, nil)
+highlightfunc('DiffChange', blue, nil, nil, nil)
+highlightfunc('DiffDelete', darkred, nil, nil, nil)
+highlightfunc('DiffText', purpleblue, nil, nil, nil)
+
+highlightfunc('EndOfBuffer', black, nil, nil, nil)
+highlightfunc('ErrorMsg', darkred, white, nil, nil)
+highlightfunc('VertSplit', white, black, nil, nil)
+highlightfunc('Folded', darkgrey, lightgrey, italic, nil)
+highlightfunc('FoldColumn', purpleblue, white, nil, nil)
+highlightfunc('SignColumn', nil, white, nil, nil)
+highlightfunc('IncSearch', black, yellow, none, nil)
+highlightfunc('Substitute', nil, skyblue, none, nil)
+highlightfunc('LineNr', darkgrey, nil , nil, nil)
+highlightfunc('CursorLineNr', yelloworange, white, nil, nil)
+highlightfunc('MatchParen', nil, lightgrey, nil, nil)
+highlightfunc('ModeMsg', green, nil, nil, nil)
+highlightfunc('MoreMsg', green, nil, nil, nil)
+highlightfunc('NonText', grey, nil, none, nil)
+highlightfunc('Normal', black, nil , nil, nil)
+linkfunc('NormalFloat', 'PMenu')
+linkfunc('NormalNC', 'PMenu')
+
+-- PMenu
+highlightfunc('PMenu', nil, offwhite, nil, nil)
+highlightfunc('PMenuSel', black, skyblue, nil, nil)
+highlightfunc('PmenuSbar', nil, darkgrey, nil, nil)
+highlightfunc('PmenuThumb', nil, lightblue, nil, nil)
+
+highlightfunc('Question', purpleblue, nil, nil, nil)
+highlightfunc('Search', nil, highlightyellow, nil, nil)
+highlightfunc('SpecialKey', green, nil, nil, nil)
+
+-- SpellCheck colors
+highlightfunc('SpellBad', nil, nil, undercurl, red)
+highlightfunc('SpellLocal', nil, nil, undercurl, turquoise)
+highlightfunc('SpellCap', nil, nil, undercurl, blue)
+highlightfunc('SpellRare', nil, nil, undercurl, teal)
+
+-- StatusLine
+highlightfunc('StatusLine', darkred, green, nil, nil)
+highlightfunc('StatusLineNC', black, darkred, nil, nil)
+
+-- Tabline
+highlightfunc('TabLine', green, darkred, nil, nil)
+highlightfunc('TabLineFill', green, darkred, nil, nil)
+highlightfunc('TabLineSel', green, darkred, nil, nil)
+
+highlightfunc('Title', purpleblue, nil, nil, nil)
+highlightfunc('Visual', black, skyblue, nil, nil)
+highlightfunc('VisualNOS', darkred, nil, nil, nil)
+highlightfunc('WarningMsg', darkred, white, nil, nil)
+highlightfunc('WildMenu', black, purpleblue, nil, nil)
+
+linkfunc('Menu', 'Normal')
+linkfunc('Scrollbar', 'PmenuSbar')
+highlightfunc('Tooltip', indigo, skyblue, nil, nil)
+
+highlightfunc('Bold', nil, nil, bold, nil)
+highlightfunc('Italic', nil, nil, italic, nil)
+
+-- Help Menu
+highlightfunc('helpExample', orange, nil, nil, nil)
+highlightfunc('helpCommand', orange, nil, nil, nil)
+
+-- Syntax Group as according to help: group-name
+highlightfunc('Comment', darkgrey, nil, italic, nil)
+
+highlightfunc('Constant', green, nil, bold, nil)
+highlightfunc('String', green, nil, nil, nil)
+highlightfunc('Character', blue, nil, nil, nil)
+highlightfunc('Number', indigo, nil, nil, nil)
+highlightfunc('Boolean', green, nil, bold, nil)
+highlightfunc('Float', teal, nil, nil, nil)
+
+highlightfunc('Identifier', lightblue, nil, nil, nil)
+highlightfunc('Function', purpleblue, nil, nil, nil)
+
+highlightfunc('Statement', purple, nil, bold, nil)
+highlightfunc('Conditional', pink, nil, bold, nil)
+highlightfunc('Repeat', brown, nil, bold, nil)
+highlightfunc('Label', pink, nil, bold, nil)
+highlightfunc('Operator', indigo, nil, nil, nil)
+highlightfunc('Keyword', darkred, nil, bold, nil)
+highlightfunc('Exception', brown, nil, bold, nil)
+
+highlightfunc('PreProc', darkblue, nil, nil, nil)
+highlightfunc('Include', purpleblue, nil, nil, nil)
+highlightfunc('Define', purple, nil, nil, nil)
+highlightfunc('Macro', darkgreen, nil, nil, nil)
+highlightfunc('PreCondit', pink, nil, nil, nil)
+
+highlightfunc('Type', indigo, nil, none, nil)
+highlightfunc('StorageClass', darkblue, nil, nil, nil)
+highlightfunc('Structure', darkblue, nil, bold, nil)
+highlightfunc('Typedef', indigo, nil, bold, nil)
+
+highlightfunc('Special', darkblue, nil, nil, nil)
+highlightfunc('SpecialChar', darkgrey, nil, nil, nil)
+highlightfunc('Tag', nil, yelloworange, nil, nil)
+highlightfunc('Delimiter', teal, nil, nil, nil)
+highlightfunc('SpecialComment', blue, nil, nil, nil)
+highlightfunc('Debug', darkred, nil, nil, nil)
+
+highlightfunc('Underlined', darkblue, nil, nil, nil)
+highlightfunc('Error', red, white, bold, nil)
+highlightfunc('Todo', green, white, bold, nil)
+
+-- from help: lsp-highlight TODO
+highlightfunc('LspDiagnosticsDefaultError', red, nil, nil, nil)
+highlightfunc('LspDiagnosticsSignError', darkred, nil, nil, nil)
+highlightfunc('LspDiagnosticsUnderlineError', nil, nil, undercurl, red)
+
+highlightfunc('LspDiagnosticsDefaultWarning', orange, nil, nil, nil)
+highlightfunc('LspDiagnosticsSignWarning', orange, nil, nil, nil)
+highlightfunc('LspDiagnosticsUnderlineWarning', nil, nil, undercurl, orange)
+
+highlightfunc('LspDiagnosticsSignInformation', purpleblue, nil, nil, nil)
+highlightfunc('LspDiagnosticsUnderlineInformation', nil, nil, undercurl, nil)
+
+highlightfunc('LspDiagnosticsSignHint', blue, nil, nil, nil)
+highlightfunc('LspDiagnosticsUnderlineHint', nil, nil, undercurl, nil)
+
+-- other links
+linkfunc('shQuote', 'String')
+linkfunc('vimScriptDelim', 'Macro')
+linkfunc('StringDelimiter', 'String')
