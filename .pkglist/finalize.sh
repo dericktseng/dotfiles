bccdefbcc51c7b2a144196a8ba081637fd10f369
Derick Tseng <derickboss1@gmail.com>
2021-06-24 11:39:43 -0700

    notes

--- .pkglist/finalize.sh
+++ .pkglist/finalize.sh
@@ -16,3 +16,11 @@ echo "please start tlp manually!"
 
 # xf86 video drivers
 echo "Make sure to install xf86-video-intel or xf86-video-amdgpu"
+
+# firefox changes
+echo "Firefox Manual about:config changes"
+echo "browser.cache.disk.enable = false"
+echo "browser.compactmode.show = true"
+echo "media.mediasource.webm.enabled = false"
+echo "media.mediasource.webm.audio.enabled = false"
+echo "and cookie related changes on privacytools.io"
