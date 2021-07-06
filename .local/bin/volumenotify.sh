3a3c28dd0a818f90dee210a970c3c28e6f2a7e30
Derick Tseng <derickboss1@gmail.com>
2021-07-06 15:29:53 -0700

    remove cursor from maim screenshot

--- .local/bin/volumenotify.sh
+++ .local/bin/volumenotify.sh
@@ -2,12 +2,13 @@
 
 volPercent="$(amixer -q -D pulse | grep Master -A 5 | tail -1| cut -d ' ' -f 7 | sed 's/[^0-9]//g')"
 hasVolume="$(amixer -q -D pulse | grep Master -A 5 | tail -1 | cut -d ' ' -f 8 | cut -d '[' -f 2 | cut -d ']' -f 1)"
-dunstID=1000
-timeout=1500
+bar=$(seq -s "─" 0 $((volPercent / 5)) | sed 's/[0-9]//g')
+
 title="Volume"
 iconSound="audio-volume-high"
 iconMuted="audio-volume-muted"
-bar=$(seq -s "─" 0 $((volPercent / 5)) | sed 's/[0-9]//g')
+dunstID=1000
+timeout=2000
 
 if [[ $hasVolume == 'on' ]]; then
 	dunstify -r $dunstID -t $timeout -i $iconSound "$title" "$volPercent% $bar"
