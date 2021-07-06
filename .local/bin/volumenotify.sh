7fe97e81596015d64ed3d1c07632d7e501ee0628
Derick Tseng <derickboss1@gmail.com>
2021-07-06 15:00:48 -0700

    fix icons

--- .local/bin/volumenotify.sh
+++ .local/bin/volumenotify.sh
@@ -4,9 +4,13 @@ volPercent="$(amixer -q -D pulse | grep Master -A 5 | tail -1| cut -d ' ' -f 7 |
 hasVolume="$(amixer -q -D pulse | grep Master -A 5 | tail -1 | cut -d ' ' -f 8 | cut -d '[' -f 2 | cut -d ']' -f 1)"
 dunstID=1000
 timeout=1500
+title="Volume"
+iconSound="audio-volume-high"
+iconMuted="audio-volume-muted"
+bar=$(seq -s "─" 0 $((volPercent / 5)) | sed 's/[0-9]//g')
 
 if [[ $hasVolume == 'on' ]]; then
-	dunstify -r $dunstID -t $timeout "Volume $volPercent%"
+	dunstify -r $dunstID -t $timeout -i $iconSound "$title" "$volPercent% $bar"
 else
-	dunstify -r $dunstID -t $timeout "Volume Muted"
+	dunstify -r $dunstID -t $timeout -i $iconMuted "$title" "Muted"
 fi
