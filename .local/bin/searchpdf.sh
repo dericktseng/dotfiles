3dd4c3df8b52e8dd78930b7d759cfaf885a03c9c
Derick Tseng <derickboss1@gmail.com>
2021-07-19 11:41:55 -0700

    script changes

--- .local/bin/searchpdf.sh
+++ .local/bin/searchpdf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fd --type f -e pdf -e epub . $HOME | rofi -keep-right -dmenu -i -p FILES -multi-select | xargs -I {} xdg-open {}
+fd --type f -e pdf -e epub . | rofi -keep-right -dmenu -matching fuzzy -sort-method fzf -sort -i -p FILES -multi-select | xargs -I {} xdg-open {}
