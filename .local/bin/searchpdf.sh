5246a03529b4da5f6c8ca8cde00097d3100e2bb9
Derick Tseng <derickboss1@gmail.com>
2021-07-31 23:54:50 -0700

    reorganize books pdf

--- .local/bin/searchpdf.sh
+++ .local/bin/searchpdf.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-fd --type f -e pdf -e epub . | rofi -keep-right -dmenu -matching fuzzy -sorting-method fzf -sort -i -p FILES -multi-select | xargs -I {} xdg-open {}
+
+bookdir="$HOME/Documents/books"
+mkdir -p $bookdir
+cd $bookdir
+fd "$@" -L -t f -e pdf -e epub | rofi -keep-right -dmenu -matching fuzzy -sorting-method fzf -sort -i -p FILES -multi-select | xargs -I {} xdg-open {}
