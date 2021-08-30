8da4aa621d6559c56fd319e1f883bab4ca57cade
Derick Tseng <derickboss1@gmail.com>
2021-08-29 23:20:30 -0700

    gradient and progressbar

--- .local/bin/brightnessnotify.sh
+++ .local/bin/brightnessnotify.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 percent=$(xbacklight -get)
-bar=$(seq -s "─" 0 $((percent / 5)) | sed 's/[0-9]//g')
 
 title="Brightness"
 icon="brightnesssettings"
 dunstID=1001
 timeout=2000
 
-dunstify -r $dunstID -t $timeout -i $icon "$title" "$percent% $bar"
+dunstify -r $dunstID -t $timeout -i $icon -h int:value:$percent "$title"
--- .local/bin/volumenotify.sh
+++ .local/bin/volumenotify.sh
@@ -2,7 +2,6 @@
 
 volPercent="$(amixer -q -D pulse | grep Master -A 5 | tail -1| cut -d ' ' -f 7 | sed 's/[^0-9]//g')"
 hasVolume="$(amixer -q -D pulse | grep Master -A 5 | tail -1 | cut -d ' ' -f 8 | cut -d '[' -f 2 | cut -d ']' -f 1)"
-bar=$(seq -s "─" 0 $((volPercent / 5)) | sed 's/[0-9]//g')
 
 title="Volume"
 iconSound="audio-volume-high"
@@ -12,7 +11,7 @@ timeout=2000
 media=$(mpvtitle.sh)
 
 if [[ $hasVolume == 'on' ]]; then
-	dunstify -r $dunstID -t $timeout -i $iconSound "$title" "$volPercent% $bar\n$media"
+	dunstify -r $dunstID -t $timeout -i $iconSound -h int:value:$volPercent "$title" "$media"
 else
 	dunstify -r $dunstID -t $timeout -i $iconMuted "$title" "Muted"
 fi
