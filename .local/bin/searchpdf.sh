0ff6d19cb4e12cc7b3804bfce7b9b31e48b54057
Derick Tseng <derickboss1@gmail.com>
2021-08-30 12:34:25 -0700

    start documenting sxhkd

--- .local/bin/searchpdf.sh
+++ .local/bin/searchpdf.sh
@@ -3,4 +3,4 @@
 bookdir="$HOME/Documents/books"
 mkdir -p $bookdir
 cd $bookdir
-fd "$@" -L -t f -e pdf -e epub | rofi -keep-right -dmenu -matching fuzzy -sorting-method fzf -sort -i -p FILES -multi-select | xargs -I {} xdg-open {}
+fd "$@" -L -t f -e pdf -e epub -e djvu | rofi -keep-right -dmenu -matching fuzzy -sorting-method fzf -sort -i -p FILES -multi-select | xargs -I {} xdg-open {}
