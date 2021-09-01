0136849cb15d9e33545be1f767192ac350e1c024
Derick Tseng <derickboss1@gmail.com>
2021-08-31 21:50:37 -0700

    change sxhkd help to monospaced font

--- .local/bin/sxhkd-help.sh
+++ .local/bin/sxhkd-help.sh
@@ -2,4 +2,4 @@
 
 awk '/^[a-z]/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
     column -t -s $'\t' |
-    rofi -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15
+    rofi -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -font "FiraCode Nerd Font Mono 12"
