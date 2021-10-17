3cb62c43d8daa2246b14150bdba6c348de227b3c
Derick Tseng <derickboss1@gmail.com>
2021-10-17 00:07:21 -0700

    update script for icons

--- .config/bspwm/desktoprename.sh
+++ .config/bspwm/desktoprename.sh
@@ -1,13 +1,69 @@
 #!/usr/bin/env bash
+namefile="$HOME/.config/bspwm/names.txt"
 
-current=$(bspc query -D -d focused --names)
-origname=$(echo "$current" | sed 's/[^0-9]//g')
-layout=$(bspc query -T -d | jq -r .userLayout)
+cutcmd="cut -d = -f 2"
+trcmd="tr -d '[:blank:]'"
+rgcmd="rg -N -m 1 -j 1"
 
-if [ $layout = "monocle" ]; then
-	# rename desktop to INTEGER+
-	bspc desktop -n "${origname}+"
-else
-	# renames desktop to INTEGER
-	bspc desktop -n "$origname"
-fi
+function rename_remove() {
+    desktopID="$1"
+    nodecount=$(bspc query --nodes -d | wc -l)
+    # rename back to number if nodecount is zero
+    if [ "$nodecount" -eq  "0" ]; then
+        firstdesktop=$(bspc query --desktops | head -n 1)
+        index=$(($desktopID - $firstdesktop + 1))
+        bspc desktop "$desktopID" --rename "$index"
+    fi
+}
+
+function rename() {
+    desktopID="$1"
+    nodeID="$2"
+    classname="$(bspc query -T -n "$nodeID" | \
+        jq -r '.client.className' | \
+        sed -e 's/[\&.^]/\\&/g')"
+    instancename="$(bspc query -T -n "$nodeID" | \
+        jq -r '.client.instanceName' | \
+        sed -e 's/[\&.^]/\\&/g')"
+    currname="$(bspc query -d "$desktopID" --names -D)"
+
+    # use instancename, then classname as fallback
+    # make sure not to include equal sign in the classname or instancename
+    newname=$($rgcmd "^${instancename}\s*=" $namefile | $cutcmd | $trcmd)
+    if [ -z "$newname" ];then
+        newname=$($rgcmd "^${classname}\s*=" $namefile | $cutcmd | $trcmd)
+    fi
+
+    # outputs unknown to /tmp/unknown.txt
+    # [ "$newname" = "null" ] && bspc query -T -n "$nodeID" >> /tmp/unknown.txt
+
+    # renames desktop if newname differs from the desktop's current name
+    if [ ! -z "$newname" ] && [ "$newname" != "$currname" ]; then
+        bspc desktop "$desktopID" --rename "$newname"
+    fi
+}
+
+bspc subscribe \
+    node_focus \
+    node_remove \
+    node_transfer | \
+    while read -r line; do
+        case "$line" in
+            node_focus* )
+                desktopID=$(echo "$line" | cut -d ' ' -f 3)
+                nodeID=$(echo "$line" | cut -d ' ' -f 4)
+                rename "$desktopID" "$nodeID"
+                ;;
+            node_transfer* )
+                desktopIDdst=$(echo "$line" | cut -d ' ' -f 6)
+                nodeID=$(echo "$line" | cut -d ' ' -f 4)
+                desktopIDsrc=$(echo "$line" | cut -d ' ' -f 3)
+                rename "$desktopIDdst" "$nodeID"
+                rename_remove "$desktopIDsrc"
+                ;;
+            node_remove* )
+                desktopID=$(echo "$line" | cut -d ' ' -f 3)
+                rename_remove "$desktopID"
+                ;;
+        esac
+    done
