321392e741ce23149b9babb6be5cdc35a77c635e
Derick Tseng <derickboss1@gmail.com>
2021-11-03 15:56:51 -0700

    use easyeffects for audio, update desktoprename script

--- .config/bspwm/desktoprename.sh
+++ .config/bspwm/desktoprename.sh
@@ -14,28 +14,24 @@ cutcmd="cut -d = -f 2"
 trcmd="tr -d '[:blank:]'"
 rgcmd="rg -N -m 1 -j 1"
 
-function rename_remove() {
+function rename_to_desktop_number() {
     desktopID="$1"
-    nodecount=$(bspc query --nodes -d "$desktopID" | wc -l)
-    # rename back to number if nodecount is zero
-    if [ "$nodecount" -eq  "0" ]; then
-        # forced to search through array because bspwm
-        # sometimes makes its desktop ID skip a number,
-        # so just getting the difference between IDs will
-        # not always work.
-        desktopList=($(bspc query -D))
-        for i in "${!desktopList[@]}";do
-            if [[ "${desktopList[i]}" = "${desktopID}" ]];then
-                bspc desktop "$desktopID" --rename "$(($i + 1))"
-            fi
-        done
-    fi
+
+    # forced to search through array because bspwm
+    # sometimes makes its desktop ID skip a number,
+    # so just getting the difference between IDs will
+    # not always work.
+    desktopList=($(bspc query -D))
+    for i in "${!desktopList[@]}";do
+        if [[ "${desktopList[i]}" = "${desktopID}" ]];then
+            bspc desktop "$desktopID" --rename "$(($i + 1))"
+            return
+        fi
+    done
 }
 
-function rename() {
-    desktopID="$1"
-    nodeID="$2"
-    currname="$(bspc query -d "$desktopID" --names -D)"
+function get_class_or_name() {
+    nodeID="$1"
     classinfo=$(xprop -id "$nodeID" | $rgcmd 'WM_CLASS')
     classname=$(echo "$classinfo" | cut -d '"' -f 2)
     class=$(echo "$classinfo" | cut -d '"' -f 4)
@@ -44,12 +40,33 @@ function rename() {
     # make sure not to include equal sign in the classname or instancename
     # the regex may need to be properly escaped.
     newname=$($rgcmd "^${class}\s*=" $namefile | $cutcmd | $trcmd)
+
     if [ -z "$newname" ];then
         newname=$($rgcmd "^${classname}\s*=" $namefile | $cutcmd | $trcmd)
     fi
+    echo "$newname"
+}
+
+function rename_remove() {
+    desktopID="$1"
+    nodecount=$(bspc query --nodes -d "$desktopID" -n '.window' | wc -l)
+    # rename back to number if nodecount is zero
+    if [ "$nodecount" -eq  "0" ]; then
+        rename_to_desktop_number "$desktopID"
+    else
+        nodeID=$(bspc query --nodes -d "$desktopID" -n '.window' | head -n 1)
+        rename "$desktopID" "$nodeID"
+    fi
+}
+
+function rename() {
+    desktopID="$1"
+    nodeID="$2"
+    currname="$(bspc query -d "$desktopID" --names -D)"
+    newname=$(get_class_or_name "$nodeID")
 
     # renames desktop if newname differs from the desktop's current name
-    if [ ! -z "$newname" ] && [ "$newname" != "$currname" ]; then
+    if [ -n "$newname" ] && [ "$newname" != "$currname" ]; then
         bspc desktop "$desktopID" --rename "$newname"
     fi
 }
