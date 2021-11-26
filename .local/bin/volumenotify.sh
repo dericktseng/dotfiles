046c450348f4ae45e8e2f6181638fe027f6757ff
Derick Tseng <derickboss1@gmail.com>
2021-11-25 18:30:06 -0800

    switch amixer for pactl for volume control

--- .local/bin/volumenotify.sh
+++ .local/bin/volumenotify.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-volPercent="$(amixer -q -D pulse | grep Master -A 5 | tail -1| cut -d ' ' -f 7 | sed 's/[^0-9]//g')"
-hasVolume="$(amixer -q -D pulse | grep Master -A 5 | tail -1 | cut -d ' ' -f 8 | cut -d '[' -f 2 | cut -d ']' -f 1)"
+sinkname="@DEFAULT_SINK@"
+volPercent="$(pactl get-sink-volume ${sinkname} | cut -d ' ' -f 6 | tr -cd '[:digit:]')"
+isMuted="$(pactl get-sink-mute ${sinkname})"
 
 title="Volume"
 iconSound="audio-volume-high"
@@ -10,7 +11,7 @@ dunstID=1000
 timeout=2000
 media=$(mpvtitle.sh)
 
-if [[ $hasVolume == 'on' ]]; then
+if [[ "$isMuted" == 'Mute: no' ]]; then
 	dunstify -r $dunstID -t $timeout -i $iconSound -h int:value:$volPercent "$title" "$media"
 else
 	dunstify -r $dunstID -t $timeout -i $iconMuted "$title" "Muted"
