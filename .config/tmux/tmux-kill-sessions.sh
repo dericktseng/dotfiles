c792c05be534369a50bf033a43793710b1bf481c
Derick Tseng <derickboss1@gmail.com>
2021-12-18 00:32:55 -0800

    allow tmux kill script to accept multiple sessions

--- .config/tmux/tmux-kill-sessions.sh
+++ .config/tmux/tmux-kill-sessions.sh
@@ -1,7 +1,10 @@
-session=$(tmux ls | fzf-tmux)
+sessions=$(tmux ls | fzf-tmux -m)
 
-if [ -n "$session" ]; then
-    name=$(echo "$session" | cut -d ':' -f 1)
-    tmux kill-session -t "$name"
-    tmux refresh-client -S
-fi
+while read session; do
+    if [ -n "$session" ]; then
+        name=$(echo "$session" | cut -d ':' -f 1)
+        tmux kill-session -t "$name"
+    fi
+done <<< "$sessions"
+
+tmux refresh-client -S
