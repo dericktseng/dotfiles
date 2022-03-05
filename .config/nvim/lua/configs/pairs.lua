8586f2d9cb11b97367ffbac14274d067cdf92c47
Derick Tseng <derickboss1@gmail.com>
2022-03-05 14:41:33 -0800

    add changes

--- .config/nvim/lua/configs/pairs.lua
+++ .config/nvim/lua/configs/pairs.lua
@@ -1,11 +1,15 @@
 local npairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
+local cond = require('nvim-autopairs.conds')
 
 -- sourced in cmp.lua to make sure this comes after
 npairs.setup{
   check_ts = true,
   enable_check_bracket_line = false,
-  fast_wrap = {},
+  fast_wrap = {
+    map = '<M-W>',
+    end_key = '$'
+  },
 }
 
 -- If you want insert `(` after select function or method item
@@ -17,15 +21,12 @@ cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex 
 cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
 
 -- you can use some built-in condition
-local cond = require('nvim-autopairs.conds')
-
-npairs.add_rule(Rule("$$", "$$", 'tex'))
 
 npairs.add_rules({
   Rule("$", "$", {"tex", "latex"})
   -- don't add a pair if the next or previous character is %
-  :with_pair(cond.not_before_regex_check("%%"))
-  :with_pair(cond.not_after_regex_check("%%"))
+  :with_pair(cond.not_before_text("$"))
+  :with_pair(cond.not_after_text("$"))
 
   -- disable add newline when press <cr>
   :with_cr(cond.none())
