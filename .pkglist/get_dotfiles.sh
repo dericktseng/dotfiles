d785c4dbf6532bfbc4d069b57e56394e84687ac6
Derick Tseng <derickboss1@gmail.com>
2022-03-12 17:21:55 -0800

    Update get_dotfiles.sh

--- .pkglist/get_dotfiles.sh
+++ .pkglist/get_dotfiles.sh
@@ -9,5 +9,5 @@ if [ "$EUID" -eq 0 ]; then
 fi
 
 git clone --bare 'https://github.com/MetriC-DT/dotfiles' $HOME/.dotfiles
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
