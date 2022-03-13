f41d0fb7cb8d8598753a1e0ee1302b6486976162
Derick Tseng <derickboss1@gmail.com>
2022-03-13 14:58:54 -0700

    add dolphin as default file manager

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -39,6 +39,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-out-alt" "<Primary>minus")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/select-by-pattern" "<Primary>s")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/rename" "F2")
+; (gtk_accel_path "<Actions>/ThunarStandardView/configure-columns" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/create-document" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/close-tab" "<Primary>w")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-type" "")
