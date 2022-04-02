0c5f98991d3f14686084a12b75ea706195f10748
Derick Tseng <derickboss1@gmail.com>
2022-04-01 22:29:38 -0700

    add imv script

--- .local/bin/imageview.sh
+++ .local/bin/imageview.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 if [ -f "$@" ]; then
     parentdir=$(dirname "$@")
-    case "$@" in
-        *.gif)  mpv --fs --loop "$@" & ;;
-        *.svg)  feh -Z -F -B black --conversion-timeout 1 -Sfilename "$parentdir" --start-at "$@" ;;
-            *)  feh -Z -F -B black --scale-down -Sfilename "$parentdir" --start-at "$@" ;;
-    esac
+    imv -r "$parentdir" -n "$@"
 fi
