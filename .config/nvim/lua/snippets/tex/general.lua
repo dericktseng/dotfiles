9c63c96a3447b9e0a1010360b3961a58ba24b57f
Derick Tseng <derickboss1@gmail.com>
2022-05-13 15:32:36 -0700

    fix broken snippets

--- .config/nvim/lua/snippets/tex/general.lua
+++ .config/nvim/lua/snippets/tex/general.lua
@@ -7,32 +7,33 @@ local c = ls.choice_node
 local d = ls.dynamic_node
 local r = ls.restore_node
 local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
 local rep = require("luasnip.extras").rep
 
 ls.add_snippets('tex', {
-  s({trig='fref', name='figure reference'}, {
-    t('Figure \\ref{fig:'), i(1), t('}')
-  }),
+  s({trig='fref', name='figure reference'},
+    fmta([[Figure \ref{fig:<>}]], i(1))
+  ),
 
-  s({trig='sref', name='section reference'}, {
-    t('Figure \\ref{sec:'), i(1), t('}')
-  }),
+  s({trig='sref', name='section reference'},
+    fmta([[Section \ref{sec:<>}]], i(1))
+  ),
 
-  s({trig='tref', name='table reference'}, {
-    t('Table \\ref{tab:'), i(1), t('}')
-  }),
+  s({trig='tref', name='table reference'},
+    fmta([[Table \ref{tab:<>}]], i(1))
+  ),
 
-  s({trig='eref', name='equation reference'}, {
-    t('Eq \\eqref{eq:'), i(1), t('}')
-  }),
+  s({trig='eref', name='equation reference'},
+    fmta([[Eq \eqref{eq:<>}]], i(1))
+  ),
 
-  s({trig='\\\\', name='newline', wordtrig=false}, {
-    t({'\\\\', ''}),
-  }),
+  s({trig='\\\\', name='newline', wordtrig=false},
+    t({'\\\\', ''})
+  ),
 
-  s({trig='...', name='ldots', wordtrig=false}, {
-    t({'\\ldots'}),
-  }),
+  s({trig='...', name='ldots', wordtrig=false},
+    t('\\ldots')
+  ),
 
   s({trig='enuma', name='enumerate (a)'}, {
     t({'\\begin{enumerate}[(a)]', '\t'}),
@@ -52,9 +53,9 @@ ls.add_snippets('tex', {
     t({'', '\\end{enumerate}'})
   }),
 
-  s({trig='qt', name='Quotation'}, {
-    t('``'), i(1), t('\'\''),
-  }),
+  s({trig='qt', name='Quotation'},
+    fmta([[``<>'']], i(1))
+  ),
 
   s({trig='mk', name='Math Inline'}, {
     t('$'), i(1), t('$'),
@@ -66,31 +67,31 @@ ls.add_snippets('tex', {
     t({'', '.\\]'}),
   }),
 
-  s({trig='si', name='siunitx'}, {
-    t('\\SI{'), i(1), t('}{'), i(2), t('}')
-  }),
+  s({trig='si', name='siunitx'},
+    fmta([[\SI{<>}{<>}]], {i(1), i(2)})
+  ),
 
   s(
     {trig='sec', name='section'},
-    fmt([[\section{{{1}}}]], i(1))
+    fmta([[\section{<>}]], i(1))
   ),
 
   s(
     {trig='al', name='align environment'},
-    fmt([[
-      \begin{{align*}}
-      {}{}
-      \end{{align*}}]],
+    fmta([[
+      \begin{align*}
+      <><>
+      \end{align*}]],
       {t('\t'), i(1)}
     )
   ),
 
   s(
     {trig='ga', name='gather environment'},
-    fmt([[
-      \begin{{gather*}}
-      {}{}
-      \end{{gather*}}]],
+    fmta([[
+      \begin{gather*}
+      <><>
+      \end{gather*}]],
       {t('\t'), i(1)}
     )
   ),
@@ -98,10 +99,10 @@ ls.add_snippets('tex', {
   -- TODO: Implement choice node for file selection for pict, source code, pdf, etc.
   s(
     {trig='pict', name='insert picture top align'},
-    fmt([[
-      \begin{{center}}
-      {}\adjustbox{{valign=t}}{{\includegraphics[width={}\textwidth]{{{}}}}}
-      \end{{center}}
+    fmta([[
+      \begin{center}
+      <>\adjustbox{valign=t}{\includegraphics[width=<>\textwidth]{<>}}
+      \end{center}
       ]],
       {t('\t'), i(1,'0.9'), i(2)}
     )
@@ -109,138 +110,138 @@ ls.add_snippets('tex', {
 
   s(
     {trig='code', name='insert code'},
-    fmt(
-      [[\lstinputlisting[language={1},basicstyle=\footnotesize]{2}]],
+    fmta(
+      [[\lstinputlisting[language=<>,basicstyle=\footnotesize]<>]],
       {i(1, 'Python'), i(2)}
     )
   ),
   
   s(
     {trig='pdf', name='insert pdf'},
-    fmt(
-      [[\includepdf[pages=-,pagecommand={{}}, width=\textwidth]{{{}.pdf}} ]],
+    fmta(
+      [[\includepdf[pages=-,pagecommand={}, width=\textwidth]{<>.pdf}]],
       {i(1)}
     )
   ),
 
   s(
     {trig='conc', name='concentration'},
-    fmt([[\ch{{[{}]}}]], {i(1)})
+    fmta([[\ch{[<>]}]], {i(1)})
   ),
 
   s(
     {trig='ch', name='chemformula', wordtrig=false},
-    fmt([[\ch{{{}}}]], {i(1)})
+    fmta([[\ch{<>}]], {i(1)})
   ),
 
   s(
     {trig='bf', name='boldface'},
-    fmt([[\textbf{{{}}}]], {i(1)})
+    fmta([[\textbf{<>}]], {i(1)})
   ),
 
   s(
     {trig='emp', name='emphasize'},
-    fmt([[\emph{{{}}}]], {i(1)})
+    fmta([[\emph{<>}]], {i(1)})
   ),
 
   s(
     {trig='it', name='italicize'},
-    fmt([[\textit{{{}}}]], {i(1)})
+    fmta([[\textit{<>}]], {i(1)})
   ),
 
   s(
     {trig='lb', name='label'},
-    fmt([[\label{{{}}}]], {i(1)})
+    fmta([[\label{<>}]], {i(1)})
   ),
 
   s(
     {trig='cite', name='citation'},
-    fmt([[\cite{{{}}}]], {i(1)})
+    fmta([[\cite{<>}]], {i(1)})
   ),
 
   s(
     {trig='eq', name='equation'},
-    fmt([[
-      \begin{{equation}}
-      {}\label{{eq:{}}}
-      {}{}
-      \end{{equation}}
+    fmta([[
+      \begin{equation}
+      <>\label{eq:<>}
+      <><>
+      \end{equation}
     ]], {t('\t'), i(1), t('\t'), i(2)})
   ),
 
   s(
     {trig='fig', name='Figure environment'},
-    fmt([[
-      \begin{{figure}}[htpb]
-      {}\centering
-      {}\includegraphics[width=0.9\textwidth]{{{}}}
-      {}\caption{{{}}}
-      {}\label{{fig:{}}}
-      \end{{figure}}
+    fmta([[
+      \begin{figure}[htpb]
+      <>\centering
+      <>\includegraphics[width=0.9\textwidth]{<>}
+      <>\caption{<>}
+      <>\label{fig:<>}
+      \end{figure}
     ]],
     {t('\t'), t('\t'), i(1), t('\t'), i(2), t('\t'), i(3)})
   ),
 
   s(
     {trig='tb', name='Table environment'},
-    fmt([[
-      \begin{{table}}[htpb]
-      {}\centering
-      {}\begin{{tabular}}{{{}}}
-      {}{}{}
-      {}\end{{tabular}}
-      {}\caption{{{}}}
-      {}\label{{tab:{}}}
-      \end{{table}}
+    fmta([[
+      \begin{table}[htpb]
+      <>\centering
+      <>\begin{tabular}{<>}
+      <><><>
+      <>\end{tabular}
+      <>\caption{<>}
+      <>\label{tab:<>}
+      \end{table}
     ]],
     {t('\t'), t('\t'), i(2), t('\t'), t('\t'), i(1), t('\t'), t('\t'), i(3), t('\t'), i(4)})
   ),
 
   s(
     {trig='beg', name='begin environment'},
-    fmt([[
-      \begin{{{1}}}{2}
-      {}{}
-      \end{{{}}}
+    fmta([[
+      \begin{<>}<>
+      <><>
+      \end{<>}
     ]],
     {i(1), i(2), t('\t'), i(3), rep(1)})
   ),
 
   s(
     {trig='setup', name='document setup'},
-    fmt([[
-          \documentclass[12pt]{{article}}
+    fmta([[
+          \documentclass[12pt]{article}
 
-          %{{{{{{ Packages 
-          \usepackage[utf8]{{inputenc}}
-          \usepackage[margin=1truein,letterpaper]{{geometry}}
-          \usepackage[shortlabels]{{enumitem}}
-          \usepackage{{amsmath}}
-          \usepackage{{amsfonts}}
-          \usepackage{{amssymb}}
-          \usepackage{{siunitx}}
-          \usepackage{{graphicx}}
-          \usepackage{{adjustbox}}
-          \usepackage{{xurl}}
-          \usepackage{{multicol}}
-          \usepackage{{listings}}
-          \usepackage{{chemformula}}
-          % \usepackage{{pdfpages}}
+          %{{{ Packages 
+          \usepackage[utf8]{inputenc}
+          \usepackage[margin=1truein,letterpaper]{geometry}
+          \usepackage[shortlabels]{enumitem}
+          \usepackage{amsmath}
+          \usepackage{amsfonts}
+          \usepackage{amssymb}
+          \usepackage{siunitx}
+          \usepackage{graphicx}
+          \usepackage{adjustbox}
+          \usepackage{xurl}
+          \usepackage{multicol}
+          \usepackage{listings}
+          \usepackage{chemformula}
+          % \usepackage{pdfpages}
 
-          \lstset{{basicstyle=\ttfamily\footnotesize,breaklines=true,showstringspaces=false}}
-          %}}}}}}
+          \lstset{basicstyle=\ttfamily\footnotesize,breaklines=true,showstringspaces=false}
+          %}}}
 
-          % \setlength{{\parindent}}{{0em}}
-          % \setlength{{\parskip}}{{0.5em}}
+          % \setlength{\parindent}{0em}
+          % \setlength{\parskip}{0.5em}
 
-          \title{{{}}}
-          \author{{{}}}
-          \date{{\today}}
+          \title{<>}
+          \author{<>}
+          \date{\today}
 
-          \begin{{document}}
-          {}\maketitle
-          {}{}
-          \end{{document}}
+          \begin{document}
+          <>\maketitle
+          <><>
+          \end{document}
     ]],
     {i(1, 'Title'), i(2, 'Derick Tseng'), t('\t'), t('\t'), i(3)})
   )
