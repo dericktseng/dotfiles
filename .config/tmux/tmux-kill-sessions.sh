da6898edc8644414992463a37fec8968015101f6
Derick Tseng <derickboss1@gmail.com>
2022-05-14 14:49:00 -0700

    fix script

--- .config/tmux/tmux-kill-sessions.sh
+++ .config/tmux/tmux-kill-sessions.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 sessions=$(tmux ls -F "#{session_name}" | fzf-tmux -m --reverse)
 session_count=$(tmux ls | wc -l)
 curr_session=$(tmux display-message -p '#S')
 
-while read session; do
+while read -r session; do
     if [ -n "$session" ]; then
         if [ "$session_count" -gt 1 ] && [ "$curr_session" = "$session" ]; then
             tmux switch-client -n
