979f497f12e9485639969b5c4bc0ef44c6a6fd5e
Derick Tseng <dericktseng@tutanota.com>
2022-07-13 16:27:54 -0700

    add print statements to c

--- .config/nvim/snippets/c.lua
+++ .config/nvim/snippets/c.lua
@@ -34,17 +34,27 @@ local regularsnips = {
 
   -- while loop
   s('while', fmta([[
-  while (<>) {
-  <><>
-  }
+    while (<>) {
+    <><>
+    }
   ]], {i(1), t('\t'), i(2)})),
 
   -- for loop
   s('for', fmta([[
-  for (<>; <>; <>) {
-  <><>
-  }
+    for (<>; <>; <>) {
+    <><>
+    }
   ]], {i(1), i(2), i(3), t('\t'), i(4)})),
+
+  -- printf statement
+  s('pr', fmta([[
+    printf("<>\n"<>);
+  ]], {i(1), d(2, utils.generate_insert_nodes, {1}, {user_args={'%%[^%%]'}})})),
+
+  -- fprintf statement
+  s('fpr', fmta([[
+    fprintf(<>, "<>\n"<>);
+  ]], {i(1, 'stderr'), i(2), d(3, utils.generate_insert_nodes, {2}, {user_args={'%%[^%%]'}})})),
 }
 
 return regularsnips
