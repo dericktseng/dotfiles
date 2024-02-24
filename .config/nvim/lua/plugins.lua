6517e2e9dbc65c750e9dfc59beb30d591d3c1d65
Derick Tseng <derickwtseng@berkeley.edu>
2024-02-23 22:23:53 -0800

    change some settings

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 -- lua plugins
 return require('packer').startup(function(use)
   use { 'hrsh7th/nvim-cmp',
@@ -52,4 +65,10 @@ return require('packer').startup(function(use)
   use 'lervag/vimtex'
   use 'tpope/vim-fugitive'
   use 'junegunn/vim-easy-align'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
