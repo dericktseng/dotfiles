19d0827ef58891b44ce39226fd07272c2d158ff1
Derick Tseng <derickwtseng@berkeley.edu>
2024-12-25 11:15:22 -0800

    Use variable fish config dir

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # sources all the files in events folder
-    for file in ~/.config/fish/events/*.fish
-        source "$file"
+    for file in $__fish_config_dir/events/*.fish
+        source $file
     end
 
     /usr/bin/zoxide init fish | source
