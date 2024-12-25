6938f7844357200e37e3a928e45b1b2ab013d53c
Derick Tseng <derickwtseng@berkeley.edu>
2024-12-24 19:04:50 -0800

    Fix fish broken aliases

--- .config/fish/functions/functions.fish
+++ .config/fish/functions/functions.fish
@@ -10,22 +10,6 @@ function fixpk
     sudo pkcon refresh force -c -1 -v -p
 end
 
-function rainbowband
-    awk 'BEGIN{
-    s="/\\/\\/\\/\\/\\"; s=s s s s s s s s;
-    for (colnum = 0; colnum<77; colnum++) {
-        r = 255-(colnum*255/76);
-        g = (colnum*510/76);
-        b = (colnum*255/76);
-        if (g>255) g = 510-g;
-        printf "\033[48;2;%d;%d;%dm", r,g,b;
-        printf "\033[38;2;%d;%d;%dm", 255-r,255-g,255-b;
-        printf "%s\033[0m", substr(s,colnum+1,1);
-    }
-    printf "\n";
-    }'
-end
-
 function mkcd
     /usr/bin/mkdir -p "$argv[0]" && cd "$argv[0]"
 end
