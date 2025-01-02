8a327b22a8d19bd11a402db2ce32b150be369b9c
Derick Tseng <derickwtseng@berkeley.edu>
2025-01-01 20:36:01 -0800

    Remove unicode

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -79,23 +79,23 @@ local plugins = {
 
 -- lazy.nvim optional configurations
 local opts = {
-  ui = {
-    icons = {
-      cmd = "⌘",
-      config = "🛠",
-      event = "📅",
-      ft = "📂",
-      init = "⚙",
-      keys = "🗝",
-      plugin = "🔌",
-      runtime = "💻",
-      require = "🌙",
-      source = "📄",
-      start = "🚀",
-      task = "📌",
-      lazy = "💤 ",
-    },
-  },
+  -- ui = {
+  --   icons = {
+  --     cmd = "⌘",
+  --     config = "🛠",
+  --     event = "📅",
+  --     ft = "📂",
+  --     init = "⚙",
+  --     keys = "🗝",
+  --     plugin = "🔌",
+  --     runtime = "💻",
+  --     require = "🌙",
+  --     source = "📄",
+  --     start = "🚀",
+  --     task = "📌",
+  --     lazy = "💤 ",
+  --   },
+  -- },
 }
 
 -- lua plugins
