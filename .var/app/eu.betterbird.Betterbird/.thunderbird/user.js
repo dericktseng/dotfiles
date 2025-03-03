f879e1193958e393b728892e4ec3af44fbf6a22c
Derick Tseng <derickwtseng@berkeley.edu>
2025-03-03 10:20:22 -0800

    user.js for thunderbird

---
+++ .var/app/eu.betterbird.Betterbird/.thunderbird/user.js
@@ -0,0 +1,23 @@
+// settings are from prefs.js
+
+user_pref("browser.cache.disk.enable", false);
+user_pref("mailnews.tags.$label1.color", "#FF0000");
+user_pref("mailnews.tags.$label1.tag", "Important");
+user_pref("mailnews.tags.$label2.color", "#FF9900");
+user_pref("mailnews.tags.$label2.tag", "Work");
+user_pref("mailnews.tags.$label3.color", "#009900");
+user_pref("mailnews.tags.$label3.tag", "Personal");
+user_pref("mailnews.tags.$label4.color", "#3333FF");
+user_pref("mailnews.tags.$label4.tag", "To Do");
+user_pref("mailnews.tags.$label5.color", "#993399");
+user_pref("mailnews.tags.$label5.tag", "Later");
+user_pref("mailnews.tags.betr.color", "#986a44");
+user_pref("mailnews.tags.betr.tag", "BETR");
+user_pref("mailnews.tags.mse_dept.color", "#dc8add");
+user_pref("mailnews.tags.mse_dept.tag", "MSE Dept");
+user_pref("mailnews.tags.version", 2);
+user_pref("mailnews.start_page.enabled", false);
+user_pref("mailnews.start_page_override.mstone", "128.7.0");
+user_pref("browser.search.region", "US");
+user_pref("mail.pane_config.dynamic", 2);
+user_pref("mail.pane_config.multiline_all", true);
