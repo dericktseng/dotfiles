854af7082c753b83491c1be8ac85355b2900023e
Derick Tseng <derickwtseng@berkeley.edu>
2025-12-02 17:46:07 -0800

    update package list

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -1,4 +1,6 @@
 // find settings in prefs.js
+
+user_pref("places.history.enabled", true);
 user_pref("privacy.resistFingerprinting", false);
 user_pref("browser.compactmode.show", true);
 user_pref("app.normandy.startupRolloutPrefs.extensions.webcompat.disabled_shims.AdvertisingDotCom", true);
