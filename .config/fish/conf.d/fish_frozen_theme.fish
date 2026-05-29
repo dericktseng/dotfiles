36109978714f3fd36d67884de1e83b43561c49fa
OceanBeagle <dwt@murena.io>
2026-05-28 17:40:30 -0700

    Use standard color scheme

---
+++ .config/fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,45 @@
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+
+set --global fish_color_autosuggestion 8e908c
+set --global fish_color_cancel --reverse
+set --global fish_color_command 8959a8
+set --global fish_color_comment eab700
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end 8959a8
+set --global fish_color_error c82829
+set --global fish_color_escape 00a6b2
+set --global fish_color_gray 
+set --global fish_color_history_current --bold
+set --global fish_color_host --reset
+set --global fish_color_host_remote yellow
+set --global fish_color_keyword 
+set --global fish_color_normal --reset
+set --global fish_color_operator 00a6b2
+set --global fish_color_option 
+set --global fish_color_param 4271ae
+set --global fish_color_quote 718c00
+set --global fish_color_redirection 3e999f
+set --global fish_color_search_match white --bold --background=brblack
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_background 
+set --global fish_pager_color_completion --reset
+set --global fish_pager_color_description B3A06D
+set --global fish_pager_color_prefix --bold --underline=single
+set --global fish_pager_color_progress brwhite --bold --background=cyan
+set --global fish_pager_color_secondary_background 
+set --global fish_pager_color_secondary_completion 
+set --global fish_pager_color_secondary_description 
+set --global fish_pager_color_secondary_prefix 
+set --global fish_pager_color_selected_background --background=brblack
+set --global fish_pager_color_selected_completion 
+set --global fish_pager_color_selected_description 
+set --global fish_pager_color_selected_prefix 
